e7a409918566025d7e199d6e0a7069ac8f4c4670
Mulah <tech.interns@jibble.io>
2023-08-31 14:46:53 +0800

    amended currency.js

--- page/ionic/scripts/currency.js
+++ page/ionic/scripts/currency.js
@@ -1,17 +1,75 @@
 const axios = require('axios');
 
-const options = {
-  method: 'GET',
-  url: 'https://currency-converter5.p.rapidapi.com/currency/convert',
-  params: {
-    format: 'json',
-    from: 'MYR',
-    to: 'AUD,EUR,IDR,SGD,USD',
-    amount: '1'
+const currencyRate = {
+  AUD: {
+    method: 'GET',
+    url: 'https://currency-converter5.p.rapidapi.com/currency/convert',
+    params: {
+      format: 'json',
+      from: 'AUD',
+      to: 'MYR',
+      amount: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': '8c0274f42amsh44d91c860b8291cp1cc65ajsnaef3874e862d',
+      'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+    },
   },
-  headers: {
-    'X-RapidAPI-Key': '8c0274f42amsh44d91c860b8291cp1cc65ajsnaef3874e862d',
-    'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+  EUR: {
+    method: 'GET',
+    url: 'https://currency-converter5.p.rapidapi.com/currency/convert',
+    params: {
+      format: 'json',
+      from: 'EUR',
+      to: 'MYR',
+      amount: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': '8c0274f42amsh44d91c860b8291cp1cc65ajsnaef3874e862d',
+      'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+    },
+  },
+  IDR: {
+    method: 'GET',
+    url: 'https://currency-converter5.p.rapidapi.com/currency/convert',
+    params: {
+      format: 'json',
+      from: 'MYR',
+      to: 'IDR',
+      amount: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': '8c0274f42amsh44d91c860b8291cp1cc65ajsnaef3874e862d',
+      'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+    },
+  },
+  SGD: {
+    method: 'GET',
+    url: 'https://currency-converter5.p.rapidapi.com/currency/convert',
+    params: {
+      format: 'json',
+      from: 'SGD',
+      to: 'MYR',
+      amount: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': '8c0274f42amsh44d91c860b8291cp1cc65ajsnaef3874e862d',
+      'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+    },
+  },
+  USD: {
+    method: 'GET',
+    url: 'https://currency-converter5.p.rapidapi.com/currency/convert',
+    params: {
+      format: 'json',
+      from: 'USD',
+      to: 'MYR',
+      amount: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': '8c0274f42amsh44d91c860b8291cp1cc65ajsnaef3874e862d',
+      'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+    },
   },
 };
 
@@ -19,10 +77,10 @@ const options = {
 async function exchangeRates() {
   try {
     const response = await axios.request(options);
-    const exchangeRantes = response.data.rates;
+    const exchangeRates = response.data.rates;
 
     // Update each row in table 
-    for (const currency in exchangeRantes) {
+    for (const currency in exchangeRates) {
       const lastPrice = document.getElementById(`${currency.toLowerCase()}-last-price`);
       if (lastPrice) {
         lastPrice.textContent = exchangeRates[currency].rate.toFixed(2);
