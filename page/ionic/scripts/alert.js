534f159733e3cf60bb37e743dd8b8268de3de5b2
Mulah <tech.interns@jibble.io>
2023-08-31 15:11:09 +0800

    updated

--- page/ionic/scripts/alert.js
+++ page/ionic/scripts/alert.js
@@ -2,4 +2,4 @@
 //    alert("Please note that this project was inspired by @ui_shreyash, and is not intended for commercial purposes or sale. Additional features on the site may be still in progress.");
 // };
 
-onload=disclaimerAlert();
+// onload=disclaimerAlert();
