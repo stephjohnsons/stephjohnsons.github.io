dc404e706d3615845c21ca55f5f44ab0863c1b2b
Mulah <tech.interns@jibble.io>
2023-08-31 15:17:39 +0800

    added script

--- page/ionic/scripts/alert.js
+++ page/ionic/scripts/alert.js
@@ -1,5 +1,5 @@
-// function disclaimerAlert() {
-//    alert("Please note that this project was inspired by @ui_shreyash, and is not intended for commercial purposes or sale. Additional features on the site may be still in progress.");
-// };
+function disclaimerAlert() {
+   alert("Please note that this project was inspired by @ui_shreyash, and is not intended for commercial purposes or sale.");
+};
 
-// onload=disclaimerAlert();
+onload=disclaimerAlert();
