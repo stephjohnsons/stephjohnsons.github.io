dfb70da65bdf2253fff69991445ae67f3e648d93
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2024-06-12 12:39:20 +0800

    Updated dev portfolio
    - changed h3 heading
    - added disclaimer as I will add case studies onto the list soon

--- src/views/VitaPages/Development.vue
+++ src/views/VitaPages/Development.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h3>Past Web Development Projects</h3>
+  <h3>Portfolio</h3>
 
   <div v-for="site in    works   " :key="site.index">
     <h6 class="mb-0 fw-semibold">{{ site.name }}</h6>
@@ -17,6 +17,19 @@
     </p>
     <hr>
   </div>
+
+  <p class="text-muted fst-italic mt-1 smaller-text">
+    <span class="fw-bold">Disclaimer: </span>
+    The content presented in this portfolio,
+    which includes but is not limited to
+    case studies and project descriptions,
+    is for illustrative purposes only.
+    All trademarks, logos, and copyrights mentioned
+    are the property of their respective owners.
+    This portfolio aims to showcase design and
+    user experience skills and does not claim
+    any affiliation or endorsement by any organization.
+  </p>
 </template>
 
 <script setup>
@@ -26,5 +39,9 @@ import { works } from "@/components/data/biographies.js";
 <style scoped>
 .fs-custom {
   font-size: 0.9rem;
+}
+
+.smaller-text {
+  font-size: 0.8rem;
 }
 </style>
