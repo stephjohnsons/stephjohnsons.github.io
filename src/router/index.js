8ed9ada64bbe2b878c8dd5eb8b158fdc9bd1854c
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2024-06-21 15:10:09 +0800

    updated SPA routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -55,7 +55,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
