95997c6cc197e405fec44120bbc68186d2fda242
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2024-06-21 15:00:58 +0800

    configure SPA support for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: () => import('../views/ProjectsView.vue')
+  },
+  {
+    path: '/bio',
+    name: 'bio',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    component: () => import('../views/BlogView.vue')
+  },
+  {
+    path: '/gallery',
+    name: 'gallery',
+    component: () => import('../views/GalleryView.vue')
+  },
+  {
+    path: '/vita',
+    name: 'vita',
+    component: () => import('../views/VitaView.vue')
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: () => import('../views/PortfolioView.vue')
+  },
+  {
+    path: '/dates',
+    name: 'dates',
+    component: () => import('../views/DatesView.vue')
+  },
+  {
+    path: '/teaching',
+    name: 'teaching',
+    component: () => import('../views/TeachingView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue')
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('../views/ProjectsView.vue')
-    },
-    {
-      path: '/bio',
-      name: 'bio',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/blog',
-      name: 'blog',
-      component: () => import('../views/BlogView.vue')
-    },
-    {
-      path: '/gallery',
-      name: 'gallery',
-      component: () => import('../views/GalleryView.vue')
-    },
-    {
-      path: '/vita',
-      name: 'vita',
-      component: () => import('../views/VitaView.vue')
-    },
-    {
-      path: '/portfolio',
-      name: 'portfolio',
-      component: () => import('../views/PortfolioView.vue')
-    },
-    {
-      path: '/dates',
-      name: 'dates',
-      component: () => import('../views/DatesView.vue')
-    },
-    {
-      path: '/teaching',
-      name: 'teaching',
-      component: () => import('../views/TeachingView.vue')
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('../views/ContactView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
