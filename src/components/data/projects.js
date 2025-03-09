79f10403e40f39a2f3af242f60ea74dea349025d
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-03-09 17:01:05 +0800

    updated projects with months

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    year: '2010',
+    year: '2010-09',
     type: ['Orchestra', 'Tour'],
     ongoing: false,
     organisation: '香港青年音樂訓練基金交流音樂會 HKMOY Exchange Programme and Tour 2010',
@@ -12,7 +12,7 @@ export const projects = [
     linkText: 'Archive'
   },
   {
-    year: '2011',
+    year: '2011-08',
     type: ['Orchestra'],
     ongoing: false,
     organisation: '香港青年音樂訓練基金亞幣音樂營 HKMOY KK Music Camp 2011',
@@ -24,7 +24,7 @@ export const projects = [
     linkText: 'Archive'
   },
   {
-    year: '2011',
+    year: '2011-12',
     type: ['Orchestra', 'Tour'],
     ongoing: false,
     organisation: '庇建中華樂團「樂定在台灣」巡迴演奏交流會 Kian Kok Chinese Orchestra Taiwan Tour',
@@ -64,7 +64,7 @@ export const projects = [
       "Kuala Lumpur City Opera promotes and stages western opera productions in Malaysia. I have performed with them in theor productions of L'Elisir d'Amore, La bohème, Madama Butterfly, La Traviata, Carmen, as well as hosting the virtual Opera Chit-Chat series in 2020."
   },
   {
-    year: '2015',
+    year: '2015-09',
     type: ['Concerts', 'Orchestra', 'Tour'],
     ongoing: false,
     organisation: 'International Youth Philharmonic Orchestra (YPHIL)',
@@ -86,7 +86,7 @@ export const projects = [
       "Played with the orchestra as a substitute tutti player in several concerts: Lambang Sari Series 1 and 2 (2024), NSO's 25th Anniversary Concert (2018), Harry Potter in Concert (2017), and many more."
   },
   {
-    year: '2016',
+    year: '2015-08',
     type: ['Recordings', 'Projects'],
     ongoing: false,
     organisation: 'Onn San • Epomania 2: Time and Space',
@@ -108,7 +108,7 @@ export const projects = [
       'A9 Strings is a piano-violin duo consisting of Dennis Law and Rohnie Tan. Together, they worked together in presenting showcases with artists, and have performed together in venues such as No Black Tie. They have worked with artists such as Michael Leaner, Elvira Arul, Nikki Palikat, Peace Teo, June Wong, Samantha Lune, Rozz, and many others.'
   },
   {
-    year: '2016',
+    year: '2016-08',
     type: ['Recordings'],
     ongoing: false,
     organisation: '凌加峻《你想得太美》',
@@ -118,7 +118,7 @@ export const projects = [
     linkText: 'Video'
   },
   {
-    year: '2016',
+    year: '2016-05',
     type: ['Musicals', 'Band'],
     ongoing: false,
     organisation: 'PAN Productions • Into the Woods by Stephen Sondheim',
@@ -127,7 +127,7 @@ export const projects = [
     description: 'Performed under the direction of Nell Ng and musical direction of Shen.'
   },
   {
-    year: '2016',
+    year: '2016-08',
     type: ['Recordings'],
     ongoing: false,
     organisation: '何芸妮 Winnie HO《一個人的自己》 Me And Myself',
@@ -137,7 +137,7 @@ export const projects = [
     linkText: 'Video'
   },
   {
-    year: '2017',
+    year: '2017-06',
     type: ['Orchestra', 'Tour'],
     ongoing: false,
     organisation: 'Miyakojima Youth International Music Festival',
@@ -146,7 +146,7 @@ export const projects = [
     description: 'Performed with Sarah Chang under Malaysian Philharmonic Youth Orchestra.'
   },
   {
-    year: '2017',
+    year: '2017-07',
     type: ['Competition', 'Chamber Music'],
     ongoing: false,
     organisation: 'Ipoh Music Festival',
@@ -156,7 +156,7 @@ export const projects = [
       'Participated the Ipoh Music Festival and Competition 2017 with pianist Cleryn Wong and violinist Keng Xin Tian. Won the first prize of chamber music category by playing Mendelssohn’s C-minor Trio.'
   },
   {
-    year: '2017',
+    year: '2017-09',
     type: ['Concerts', 'Orchestra', 'Tour'],
     ongoing: false,
     organisation: 'World Youth Symphony Orchestra',
@@ -178,7 +178,7 @@ export const projects = [
       'Premier chamber orchestra in Kuala Lumpur. I was the Vice President (2019-2021; later acting President till July 2023) of the organisation. During my term, I succeeded in a partnership deal with UCSI University where the ensemble was appointed as the ensemble-in-residence and produced around 10 concerts with the ex-President, Foo Yin Hong. In those concerts, the ensemble worked with the Alliance Française KL/Le French Festival, Dominus Arts Venue, and artists such as Dr Ang Mei Foong, Dr Poom Prommachart, Maïa Darmé, Carlos Andres Mejías, Bryan Lee.'
   },
   {
-    year: '2018',
+    year: '2018-08',
     type: ['Concerts', 'Orchestra'],
     ongoing: false,
     organisation: 'Siam Sinfonietta',
@@ -190,7 +190,7 @@ export const projects = [
     linkText: 'Video'
   },
   {
-    year: '2018',
+    year: '2018-07',
     type: ['Competition', 'Chamber Music'],
     ongoing: false,
     organisation: 'Festival de Musique des Arcs',
@@ -200,7 +200,7 @@ export const projects = [
       "DSCH Quartet, the only participating ensemble from Malaysia at the festival, won the prize for chamber music with their performance of Debussy's String Quartet."
   },
   {
-    year: '2018',
+    year: '2018-07',
     type: ['Concerts', 'Projects', 'Band'],
     ongoing: false,
     organisation: "PAN Productions • That's Entertainment 2018",
@@ -209,7 +209,7 @@ export const projects = [
     description: 'Performed under the direction of Nell Ng and musical direction of Dennis Law.'
   },
   {
-    year: '2019',
+    year: '2019-05',
     type: ['Chamber Music', 'Tour'],
     ongoing: false,
     organisation: '8H Quartet',
@@ -229,7 +229,7 @@ export const projects = [
       'Tarakucha Band: A Malaysian big band with a vintage twist, featuring singers Sean Ghazi and Ida Mariana, with musical direction by Nish Tham. Played in annual productions and one commercial concert in Langkawi Island.'
   },
   {
-    year: '2019',
+    year: '2019-10',
     type: ['Concerts', 'Projects', 'Band'],
     ongoing: false,
     organisation: "PAN Productions • That's Entertainment 2019",
@@ -239,7 +239,7 @@ export const projects = [
       'Performed under the direction of Nell Ng and musical direction of Dennis Law as a cellist.'
   },
   {
-    year: '2019',
+    year: '2019-07',
     type: ['Musicals', 'Band'],
     ongoing: false,
     organisation: 'PAN Productions • Next to Normal the Musical',
@@ -249,7 +249,7 @@ export const projects = [
       'Tony award winning Broadway musical hailed as "...one of the best musicals of the 21st century" by the New York Times. Performed under the direction of Nell Ng and musical direction of Dennis Law.'
   },
   {
-    year: '2019',
+    year: '2019-02',
     type: ['Recordings'],
     ongoing: false,
     organisation: 'Crinkle Cut • Goodbye',
@@ -259,7 +259,7 @@ export const projects = [
     linkText: 'Video'
   },
   {
-    year: '2019',
+    year: '2018-12',
     type: ['Concert', 'Recordings', 'Collaboration'],
     ongoing: false,
     organisation: '阿德 张盛德 2018《天命 51》',
@@ -269,7 +269,7 @@ export const projects = [
       "Played for Chang Seng Teck's showcase concert under the direction of Chong Eugene."
   },
   {
-    year: '2019',
+    year: '2019-05',
     type: ['Residency'],
     ongoing: false,
     organisation: 'Scandinavian Cello School',
@@ -289,7 +289,7 @@ export const projects = [
       'Performed in 5 cities in Northern China: Yinchuan Theater, Ningxia Theater, Alxa, Ordos, and Baotou with musicians from different orchestras.'
   },
   {
-    year: '2020',
+    year: '2020-02',
     type: ['Recordings'],
     ongoing: false,
     organisation: '傅健穎《領空》 Jyin Poh • Fault in Our Space',
@@ -299,7 +299,7 @@ export const projects = [
     linkText: 'Video'
   },
   {
-    year: '2020',
+    year: '2020-02',
     type: ['Masterclass', 'Tour', 'Teaching'],
     ongoing: false,
     organisation: 'Century Music School',
@@ -319,17 +319,29 @@ export const projects = [
       "Referred to as the 'Sabahan Songbird', Mayabayu is a powerful songstress that combines synth-pop with tribal elements of East Malaysia. Previously known as Beverly Matujal, the singer-songwriter has won a Global VIMA Award for Overall Best Female Artiste in 2018. I have recorded for her two songs, 'Huminodun' and 'O, Sayang', as well as performed with her in the Levi's Music Projects Showcase in 2020."
   },
   {
-    year: '2021',
+    year: '2021-06',
     type: ['Teaching'],
     ongoing: false,
     organisation: 'Lahad Datu Middle School',
     location: 'Lahad Datu (virtual)',
     cc: 'MY',
+    link: 'https://www.ucsiuniversity.edu.my/sites/default/files/4-imus_newsletter_vol.4.pdf',
     description:
       'Conducted a virtual class on music theory for students of the Lahad Datu Middle School.'
   },
   {
-    year: '2021',
+    year: '2021-06',
+    type: ['Community', 'Projects'],
+    ongoing: false,
+    organisation: 'SEA Music Academy • Money and Musician',
+    location: 'Lahad Datu (virtual)',
+    cc: 'MY',
+    link: 'https://www.ucsiuniversity.edu.my/sites/default/files/4-imus_newsletter_vol.4.pdf',
+    description:
+      'A talk between Dr Andrew Filmer and Stephen Johnson-Tseu aimed at dealing with the tricky subject of money and musicians, which included conversations on earning a living as a musician, the current industry rates, funding sources, and current issues and hurdles in the music industry and the topic of when and if a musician should consider to provide their service free of charge. '
+  },
+  {
+    year: '2021-11',
     type: ['Chamber Music', 'Recital'],
     ongoing: false,
     organisation: 'Artist-in-Residence Chamber Concert, UCSI University',
@@ -339,14 +351,14 @@ export const projects = [
       "Performed as a guest cellist in an All-Schumann Chamber Concert with artist-in-residence, Ng Chong Lim, at the UCSI University's Institute of Music."
   },
   {
-    year: '2021',
+    year: '2021-2023',
     type: ['Projects', 'Chamber Music', 'Tour'],
     ongoing: false,
     organisation: 'heART • Interdisciplinary Research on "Social Identity"',
     location: 'Germany',
     cc: 'DE',
     description:
-      '"heART" is an art collective founded by German pianist Daniel Bucurescu and Malaysian cellist Stephen Johnson-Tseu. As a creative ensemble, they seek to redefine classical music through interactions with society to ensure constant encounters and engagement between music and the people. For this purposes, they focus mainly on: (1) interdisciplinary performances; (2) education projects; and (3) workshops on creativity, dramaturgy, and concert design.'
+      '"heART" is an art collective founded by German pianist Daniel Bucurescu and Malaysian cellist Stephen Johnson-Tseu in 2022. As a creative ensemble, they seek to redefine classical music through interactions with society to ensure constant encounters and engagement between music and the people. For this purposes, they focus mainly on: (1) interdisciplinary performances; (2) education projects; and (3) workshops on creativity, dramaturgy, and concert design.'
   },
   {
     year: '2022-2023',
@@ -359,7 +371,7 @@ export const projects = [
       "Performs together as a piano quintet together with fellow faculty members: Senior Prof. Dr. P'ng Tean Hwa, Jerry Lee Sing Hong, Wani Ismail, and Cheryl Choi. Also performed in Medan, Indonesia as part of their tour to Medan Muzik in 2023."
   },
   {
-    year: '2022',
+    year: '2022-09',
     type: ['Concerts', 'Orchestra'],
     ongoing: false,
     organisation: 'Denver Philharmonic Orchestra',
@@ -369,7 +381,7 @@ export const projects = [
       "Performed Beethoven's Symphony No. 9 with the Denver Philharmonic Orchestra and the Bayerische Philharmonie Chorus, Antonia Bourvé, Luise Heiß, Andrew Lepri Meyer, Thomas Gropper, Javelyn Kryeziu, and the BayPhil Junior Strings at the Hercules Hall, Residenz Munich."
   },
   {
-    year: '2023',
+    year: '2023-03',
     type: ['Residency', 'Tour'],
     ongoing: false,
     organisation: 'Mas Palou Artist Residency',
@@ -379,7 +391,7 @@ export const projects = [
       'Participated in a short artist residency at the Mas Palou Artist Residency with German pianist Daniel Bucurescu, working on their heART Projects together. Also recorded a short video excerpt at the Goethe-Institut Barcelona.'
   },
   {
-    year: '2023',
+    year: '2023-12',
     type: ['Concerts', 'Tour', 'Chamber Music'],
     ongoing: false,
     organisation: 'Clarinet Quintet & Piano',
@@ -389,7 +401,7 @@ export const projects = [
       "Performed Mozart's quintet and Prokofiev's Overture on Hebrew Themes in 3 different venues around Johor with Ng Eng Hao, Tan Guang Yu, Tong Choon Ho, Eben Ting, and Lim Yu Jiun."
   },
   {
-    year: '2023',
+    year: '2023-12',
     type: ['Concerts', 'Tour', 'Band'],
     ongoing: false,
     organisation: 'Nathania Ong • Spirit of Giving - A Musical Homecoming',
@@ -407,7 +419,7 @@ export const projects = [
     description: 'Led the sectionals for the klpac String Orchestra in two separate occasions.'
   },
   {
-    year: '2024',
+    year: '2024-04',
     type: ['Community', 'Orchestra'],
     ongoing: false,
     organisation: 'USM Kelantan Music Society',
@@ -427,7 +439,7 @@ export const projects = [
       'Managed and curated multiple events and performances for the Peninsular Festival Orchestra under Red Carpet Artist Management. Past projects included ESL Dota 2 Malaysia 2022 & 2023, Heineken Refresh 2024 (set with DJ Blink), and Disney in Concert Malaysia (May, July 2023, Jan 2024).'
   },
   {
-    year: '2024',
+    year: '2024-06',
     type: ['Concerts'],
     ongoing: false,
     organisation: 'Naan Pradeep Kumar',
@@ -437,7 +449,7 @@ export const projects = [
       'Performed with Pradeep Kumar in the orchestra under the baton of Datuk Mustafa Fuzer bin Nawi.'
   },
   {
-    year: '2024',
+    year: '2024-07',
     type: ['Concerts', 'Tour'],
     ongoing: false,
     organisation: 'This is Johan - Live in Singapore',
@@ -446,7 +458,7 @@ export const projects = [
     description: "Hip-Hop singer Joe Flizzow's concert in Singapore."
   },
   {
-    year: '2024',
+    year: '2024-11',
     type: ['Concerts'],
     ongoing: false,
     organisation: 'MONO Asia Tour: Kuala Lumpur',
@@ -455,7 +467,7 @@ export const projects = [
     description: ''
   },
   {
-    year: '2024',
+    year: '2024-12',
     type: ['Concerts', 'Tour'],
     ongoing: false,
     organisation: 'Spirit of Giving: Corey Koh',
