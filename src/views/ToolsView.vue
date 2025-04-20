5ca764a64a59f49b6fb0736dc2b639d393391e4d
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-04-20 17:14:17 +0800

    took out password page - added terms and conditions - added tools view

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -14,29 +14,13 @@
       </p>
     </div>
   </div>
-  <div v-else class="d-flex flex-column align-items-center justify-content-center vh-100">
-    <input v-model="inputPassword" type="password" class="form-control w-25 mb-3" placeholder="Enter password" />
-    <button class="btn btn-primary" @click="checkPassword" @enter="checkPassword">Submit</button>
-  </div>
+  <PasswordPage v-else />
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import PasswordPage from '@/components/PasswordPage.vue';
 import ZhTwConverter from '@/components/ZhTwConverter.vue';
 import TimeZoneConverter from '@/components/TimeZoneConverter.vue';
 import CurrencyConverter from '@/components/CurrencyConverter.vue';
 import CurrentTime from '@/components/CurrentTime.vue';
-
-const authenticated = ref(localStorage.getItem('authenticated') === 'true');
-const inputPassword = ref('');
-
-const checkPassword = () => {
-  if (inputPassword.value === import.meta.env.VITE_TOOLS_PASSWORD) {
-    authenticated.value = true;
-    localStorage.setItem('authenticated', 'true');
-  } else {
-    alert('Incorrect password');
-  }
-};
-
 </script>
