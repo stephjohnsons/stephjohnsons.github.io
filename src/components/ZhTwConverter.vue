42a9d7868a58c40139f77327a24272983525151d
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-04-25 08:50:22 +0800

    updated

--- src/components/ZhTwConverter.vue
+++ src/components/ZhTwConverter.vue
@@ -43,6 +43,7 @@
     <button class="btn btn-sm btn-outline-secondary" @click="templates.educate.defender">屏蔽</button>
     <button class="btn btn-sm btn-outline-secondary" @click="templates.educate.specify">指定</button>
     <button class="btn btn-sm btn-outline-secondary" @click="templates.search">搜索</button>
+    <button class="btn btn-sm btn-outline-secondary" @click="templates.educate.instantBook">闪订</button>
     <button class="btn btn-sm btn-outline-secondary" @click="templates.educate.aircover">诶卡</button>
     <button class="btn btn-sm btn-outline-danger" @click="templates.closing.zh1(); hideTw = true">结一</button>
     <button class="btn btn-sm btn-outline-danger" @click="templates.closing.zh2(); hideTw = true">结二</button>
@@ -141,7 +142,7 @@ const allTemplates = {
     ],
     educate: [
       'aircover', 'delay', 'defender', 'followup', 'feedback',
-      'probe', 'fapiao', 'waiting', 'international', 'specify', 'noReply'
+      'probe', 'fapiao', 'waiting', 'international', 'specify', 'noReply', 'instantBook'
     ],
     reso: ['G', 'H', 'M'],
     closing: ['p', 'zh1', 'zh2', 'tw1', 'tw2']
