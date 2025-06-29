9e9838f3965a31c824e9e442e81f943e3d9029ba
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-06-29 15:53:08 +0800

    updated classes in router

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,11 @@ const routes = [
     component: () => import('../views/VitaView.vue')
   },
   {
+    path: '/classes',
+    name: 'Classes',
+    component: () => import('../views/ClassesView.vue')
+  },
+  {
     path: '/dates',
     name: 'dates',
     component: () => import('../views/DatesView.vue')
