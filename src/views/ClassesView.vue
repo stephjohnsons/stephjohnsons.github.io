9e4af108e3c8ef74f87001061ddf28b73e88dc10
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-07-01 22:48:28 +0800

    responsive view

--- src/views/ClassesView.vue
+++ src/views/ClassesView.vue
@@ -1,11 +1,19 @@
 <template>
   <div v-if="adminAuthenticated || studentAuthenticated" class="d-flex">
-    <div class="col-10">
+    <div class="col-12 col-md-10">
       <StudentClassesTracker v-if="adminAuthenticated" />
       <LessonList />
       <RepertoireList />
+      <div class="footer sticky-bottom rounded-3 px-2 pt-2 d-flex d-md-none bg-warning">
+        <div class="d-flex flex-row mb-2">
+          <button v-if="adminAuthenticated" class="btn btn-sm btn-outline-dark m-1"
+            @click="scrollToStudents()">Students</button>
+          <button class="btn btn-sm btn-outline-dark m-1" @click="scrollToClasses()">Classes</button>
+          <button class="btn btn-sm btn-outline-dark m-1" @click="scrollToRepertoire()">Rep</button>
+        </div>
+      </div>
     </div>
-    <div class="position-fixed top-25 end-0 me-4 pe-1 text-end">
+    <div class="position-fixed top-25 end-0 me-4 pe-1 text-end d-none d-md-block">
       <h5>Sitemap</h5>
       <div class="d-flex flex-column">
         <button v-if="adminAuthenticated" class="btn btn-sm btn-warning my-1"
