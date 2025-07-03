7dfe9730e5456b1906d88fa3b8c6e76510e7c1dc
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-07-03 21:48:31 +0800

    delete utils folder and file

--- src/utils/getStudentName.js
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue'
-
-const students = ref([])
-
-export const studentName = (id) => {
-  const student = students.value.find((s) => s.id === id)
-  return student ? student.student : 'Unknown'
-}
