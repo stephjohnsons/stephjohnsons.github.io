59c01552f80d2cb2ba7a5e7c308eece0d11b3f8e
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-11-24 14:33:58 +0800

    updated downloadsview

--- src/views/DownloadsView.vue
+++ src/views/DownloadsView.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="file-browser">
     <h1>Downloadable Files</h1>
+    <p>
+      These files were materials I used for Music History 101 (Middle Ages to early Classical period). <br />
+      Feel free to let me know if there are any mistakes or incorrect facts.
+    </p>
     <!--
       Source - https://stackoverflow.com/a
       Posted by Chema, modified by community. See post 'Timeline' for change history
       Retrieved 2025-11-24, License - CC BY-SA 4.0
     -->
 
-    <iframe
-      src="https://drive.google.com/a/stephjohnsons.com/embeddedfolderview?id=1_9Uhvplnpi9aIw0Nb3GtmAW3ar6ELSIt#list"
-      style="width:100%; height:600px; border:0;"></iframe>
+    <div class="border border-2 rounded">
+      <iframe
+        src="https://drive.google.com/a/stephjohnsons.com/embeddedfolderview?id=1_9Uhvplnpi9aIw0Nb3GtmAW3ar6ELSIt#list"
+        style="width:100%; height:600px;"></iframe>
+    </div>
   </div>
 </template>
 
