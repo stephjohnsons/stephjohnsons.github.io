4dbdfe1527da90c016f41da5107f9f88ac9368bb
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2025-11-24 14:29:22 +0800

    updated - downloadsview with gdrivce embed

--- src/views/DownloadsView.vue
+++ src/views/DownloadsView.vue
@@ -1,51 +1,24 @@
 <template>
   <div class="file-browser">
-    <h2>Folder Files</h2>
-    <ul>
-      <li v-for="file in files" :key="file.id">
-        <a v-if="file.mimeType === 'application/pdf' || file.mimeType.includes('image')" :href="file.webContentLink"
-          target="_blank" rel="noopener noreferrer">
-          {{ file.name }}
-        </a>
-        <span v-else>{{ file.name }}</span>
-      </li>
-    </ul>
+    <h1>Downloadable Files</h1>
+    <!--
+      Source - https://stackoverflow.com/a
+      Posted by Chema, modified by community. See post 'Timeline' for change history
+      Retrieved 2025-11-24, License - CC BY-SA 4.0
+    -->
+
+    <iframe
+      src="https://drive.google.com/a/stephjohnsons.com/embeddedfolderview?id=1_9Uhvplnpi9aIw0Nb3GtmAW3ar6ELSIt#list"
+      style="width:100%; height:600px; border:0;"></iframe>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-
-const files = ref([]);
-const backend = import.meta.env.VITE_TEMPLATE_BACKEND_API_URL;
-
-import { fetchDriveFiles } from '../components/data/drive.js';
-
-async function showFiles() {
-  const files = await fetchDriveFiles();
-  files.forEach(file => {
-    console.log(file.name, file.webViewLink, file.webContentLink);
-  });
-}
-
-showFiles();
-
-onMounted(async () => {
-  try {
-    const res = await fetch(`${backend}/drive/files`);
-    const data = await res.json();
-    files.value = data;
-  } catch (err) {
-    console.error('Failed to fetch files', err);
-  }
-});
 </script>
 
 <style scoped>
 .file-browser {
-  max-height: 400px;
   overflow-y: auto;
-  border: 1px solid #ccc;
   padding: 1rem;
 }
 </style>
