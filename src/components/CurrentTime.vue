6f8c8a64a4c4b43525198e0d787fd78e3251a847
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2026-05-29 21:28:26 +0800

    updated time

--- src/components/CurrentTime.vue
+++ src/components/CurrentTime.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="fs-4">
-    <h2>{{ currentTime }}</h2>
+    <h2
+      class="fw-light"
+      id="clock"
+    >{{ currentTime }}</h2>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import moment from 'moment';
+import { useUIStore } from '@/stores/ui';
 
 const currentTime = ref(moment().format('HH:mm:ss'));
+const ui = useUIStore();
 
 let interval;
 
@@ -22,3 +27,9 @@ onUnmounted(() => {
   clearInterval(interval);
 });
 </script>
+
+<style scoped>
+.dark-mode #clock {
+  color: #fd890d;
+}
+</style>
