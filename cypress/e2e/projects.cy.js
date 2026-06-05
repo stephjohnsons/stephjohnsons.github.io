5fee0934edb61add3730113a07da69131fa34c47
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2026-06-05 16:54:00 +0800

    Comment out tests in projects.cy.js
    Comment out existing test cases for projects page.

--- cypress/e2e/projects.cy.js
+++ cypress/e2e/projects.cy.js
@@ -1,64 +1,64 @@
-it('loads projects page', () => {
-  cy.visit('/projects')
+// it('loads projects page', () => {
+//   cy.visit('/projects')
 
-  cy.contains('Past Musical Projects and Concerts')
-})
+//   cy.contains('Past Musical Projects and Concerts')
+// })
 
-it('loads project data', () => {
-  cy.visit('/projects')
+// it('loads project data', () => {
+//   cy.visit('/projects')
 
-  cy.contains('Loading projects...').should('not.exist')
+//   cy.contains('Loading projects...').should('not.exist')
 
-  cy.get('h5').should('have.length.greaterThan', 0)
-})
+//   cy.get('h5').should('have.length.greaterThan', 0)
+// })
 
-// Ongoing projects
-it('filters ongoing projects', () => {
-  cy.visit('/projects')
+// // Ongoing projects
+// it('filters ongoing projects', () => {
+//   cy.visit('/projects')
 
-  cy.contains('button', 'Ongoing')
-    .click()
+//   cy.contains('button', 'Ongoing')
+//     .click()
 
-  cy.contains('Ongoing')
-    .should('exist')
-})
+//   cy.contains('Ongoing')
+//     .should('exist')
+// })
 
-it('shows ongoing badge for ongoing projects', () => {
-  cy.visit('/projects')
+// it('shows ongoing badge for ongoing projects', () => {
+//   cy.visit('/projects')
 
-  cy.contains('.badge', 'Ongoing')
-})
+//   cy.contains('.badge', 'Ongoing')
+// })
 
-// Clears filters 
-it('clears all filters', () => {
-  cy.visit('/projects')
+// // Clears filters 
+// it('clears all filters', () => {
+//   cy.visit('/projects')
 
-  cy.get('select').select('Teaching')
+//   cy.get('select').select('Teaching')
 
-  cy.contains('button', 'Clear all filters')
-    .click()
+//   cy.contains('button', 'Clear all filters')
+//     .click()
 
-  cy.get('select')
-    .should('have.value', '')
-})
+//   cy.get('select')
+//     .should('have.value', '')
+// })
 
-// Password modal for admin
-it('opens auth modal', () => {
-  cy.visit('/projects')
+// // Password modal for admin
+// it('opens auth modal', () => {
+//   cy.visit('/projects')
 
-  cy.contains('Edit')
-    .click()
+//   cy.contains('Edit')
+//     .click()
 
-  cy.contains('Enter password to edit')
-})
+//   cy.contains('Enter password to edit')
+// })
 
-it('closes auth modal', () => {
-  cy.visit('/projects')
+// it('closes auth modal', () => {
+//   cy.visit('/projects')
 
-  cy.contains('Edit').click()
+//   cy.contains('Edit').click()
 
-  cy.contains('Cancel').click()
+//   cy.contains('Cancel').click()
 
-  cy.contains('Enter password to edit')
-    .should('not.exist')
-})
+//   cy.contains('Enter password to edit')
+//     .should('not.exist')
+// })
