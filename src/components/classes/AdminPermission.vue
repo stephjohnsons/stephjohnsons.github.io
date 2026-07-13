7db77a07588ff3813cbe160a00694172b858ed57
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2026-07-13 19:01:21 +0800

    updated admin/student view and removed sitemap links

--- src/components/classes/AdminPermission.vue
+++ src/components/classes/AdminPermission.vue
@@ -1,108 +1,29 @@
 <template>
-  <div class="col-12 col-lg-10 d-none d-lg-block">
-    <div class="d-flex g-2">
-      <div class="col-6 me-2">
-        <StudentList
-          mode="admin"
-          id="students"
-        />
-      </div>
+  <div class="col-12 d-none d-lg-block">
+    <div class="d-flex gap-4">
       <div class="col-6">
-        <LessonList
-          mode="admin"
-          id="lessons"
-        />
+        <StudentList mode="admin" />
+      </div>
+      <div class="col-6 pe-5">
+        <LessonList mode="admin" />
       </div>
     </div>
-    <RepertoireList
-      id="repertoire"
-      mode="admin"
-    />
-    <div class="footer sticky-bottom rounded-3 px-2 pt-2 d-flex d-lg-none bg-warning">
-      <div class="d-flex flex-row mb-2">
-        <button
-          v-for="item in navItems"
-          :key="item.id"
-          class="btn btn-sm btn-outline-dark m-1"
-          @click="scrollToSection(item.id)"
-        >
-          {{ item.label }}
-        </button>
-      </div>
-    </div>
+    <RepertoireList mode="admin" />
 
     <div class="border-top pt-4">
       Class policy is only available on student view.
     </div>
   </div>
   <div class="d-block d-lg-none">
-    <StudentList id="students" />
-    <LessonList id="lessons" />
-    <RepertoireList id="repertoire" />
-    <div class="footer sticky-bottom rounded-3 px-2 pt-2 d-flex d-lg-none bg-warning">
-      <div class="d-flex flex-row mb-2">
-        <button
-          v-for="item in navItems"
-          :key="item.id"
-          class="btn btn-sm btn-outline-dark m-1"
-          @click="scrollToSection(item.id)"
-        >
-          {{ item.label }}
-        </button>
-
-      </div>
-    </div>
-  </div>
-  <div class="position-fixed top-25 end-0 me-4 pe-1 text-end d-none d-lg-block">
-    <h5>Sitemap</h5>
-    <div class="d-flex flex-column">
-      <button
-        class="btn btn-sm btn-warning my-1"
-        @click="scrollToSection('students')"
-      >
-        Students
-      </button>
-      <button
-        class="btn btn-sm btn-warning my-1"
-        @click="scrollToSection('lessons')"
-      >
-        Classes
-      </button>
-      <button
-        class="btn btn-sm btn-warning my-1"
-        @click="scrollToSection('repertoire')"
-      >
-        Rep
-      </button>
-    </div>
+    <StudentList mode="admin" />
+    <LessonList mode="admin" />
+    <RepertoireList mode="admin" />
   </div>
 </template>
 
 
 <script setup>
-import { computed } from "vue";
 import StudentList from "../StudentList.vue";
 import LessonList from "../LessonList.vue";
 import RepertoireList from "../RepertoireList.vue";
-
-const navItems = computed(() => {
-  const items = [];
-
-  items.push(
-    {
-      label: "Classes",
-      id: "lessons",
-    },
-    {
-      label: "Rep",
-      id: "repertoire",
-    },
-    {
-      label: "Policy",
-      id: "policy",
-    }
-  );
-
-  return items;
-});
 </script>
--- src/components/classes/StudentPermission.vue
+++ src/components/classes/StudentPermission.vue
@@ -1,58 +1,26 @@
 <template>
-  <div class="col-12 col-lg-10 d-none d-lg-block">
-    <div class="d-flex g-2">
-      <div
-        class="col-6 me-2"
-        id="students"
-      >
+  <div class="col-12 d-none d-lg-block">
+    <div class="d-flex gap-4">
+      <div class="col-6">
         <StudentList mode="student" />
       </div>
-      <div
-        class="col-6"
-        id="lessons"
-      >
+      <div class="col-6 pe-5">
         <LessonList mode="student" />
       </div>
     </div>
-    <RepertoireList
-      id="repertoire"
-      mode="student"
-    />
-    <div class="footer sticky-bottom rounded-3 px-2 pt-2 d-flex d-lg-none bg-warning">
-      <div class="d-flex flex-row mb-2">
-        <button
-          v-for="item in navItems"
-          :key="item.id"
-          class="btn btn-sm btn-outline-dark m-1"
-          @click="scrollToSection(item.id)"
-        >
-          {{ item.label }}
-        </button>
-      </div>
-    </div>
-
+    <RepertoireList mode="student" />
     <div class="border-top pt-4">
       <ClassPolicy />
     </div>
   </div>
   <div class="d-block d-lg-none">
     <StudentList
-      id="students"
       mode="student"
       v-if="!studentAuthenticated"
     />
-    <LessonList
-      id="lessons"
-      mode="student"
-    />
-    <RepertoireList
-      id="repertoire"
-      mode="student"
-    />
-    <div
-      class="border-top pt-4"
-      id="policy"
-    >
+    <LessonList mode="student" />
+    <RepertoireList mode="student" />
+    <div class="border-top pt-4">
       <ClassPolicy />
     </div>
     <div class="footer sticky-bottom rounded-3 px-2 pt-2 d-flex d-lg-none bg-warning">
@@ -69,40 +37,10 @@
       </div>
     </div>
   </div>
-  <div class="position-fixed top-25 end-0 me-4 pe-1 text-end d-none d-lg-block">
-    <h5>Sitemap</h5>
-    <div class="d-flex flex-column">
-      <button
-        v-if="!studentAuthenticated"
-        class="btn btn-sm btn-warning my-1"
-        @click="scrollToSection('students')"
-      >
-        Students
-      </button>
-      <button
-        class="btn btn-sm btn-warning my-1"
-        @click="scrollToSection('lessons')"
-      >
-        Classes
-      </button>
-      <button
-        class="btn btn-sm btn-warning my-1"
-        @click="scrollToSection('repertoire')"
-      >
-        Rep
-      </button>
-      <button
-        class="btn btn-sm btn-warning my-1"
-        @click="scrollToSection('policy')"
-      >
-        Policy
-      </button>
-    </div>
-  </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { onMounted } from "vue";
 import ClassPolicy from "../ClassPolicy.vue";
 import StudentList from "../StudentList.vue";
 import LessonList from "../LessonList.vue";
@@ -115,33 +53,6 @@ const props = defineProps({
     default: null
   }
 })
-
-const navItems = computed(() => {
-  const items = [];
-
-  items.push(
-    {
-      label: "Classes",
-      id: "lessons",
-    },
-    {
-      label: "Rep",
-      id: "repertoire",
-    },
-    {
-      label: "Policy",
-      id: "policy",
-    }
-  );
-
-  return items;
-});
-
-const scrollToSection = (id) => {
-  document.getElementById(id)?.scrollIntoView({
-    behavior: "smooth",
-  });
-};
 
 const studentStore = useStudentStore();
 onMounted(async () => {
